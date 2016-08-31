d1f11069ac00a287b00bcd1a8676d6f6dad50508
Алексей Горбель < >
2016-08-31 10:39:42 +0300

    Create repositiry. Add google maps in proj

---
+++ Controllers/JorneyController.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../Scripts/angular.js" />
+/// <reference path="../Scripts/jquery-3.1.0.js" />
+
+var jorneyApp = angular.module("jorneyApp", []);
+jorneyApp.controller("JorneyController", ["$scope", "$http", JorneyCtrl]);
+
+function JorneyCtrl ($scope, $http) {
+    $scope.busIds = null;
+    $scope.getBusIds = function () {
+        $http.get("https://api.tfl.gov.uk/Line/Mode/bus?app_id=&app_key=")
+        .success(function (data) {
+            $scope.busIds = data;
+        })
+        .error(function (data) {
+            console.log(data);
+        });
+    }
+    
+}
---
+++ Controllers/map.js
@@ -0,0 +1,7 @@
+﻿function initMap() {
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+        center: { lat: 51.528837, lng: -0.165653 },
+        zoom: 10
+    });
+}
