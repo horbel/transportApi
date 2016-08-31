98d7b915288602e0f451cef908118d6e47c010d9
gorbel <gorbel.alex@ya.ru>
2016-09-01 00:49:09 +0300

    Add styles.Implement table with routes names

--- Controllers/JorneyController.js
+++ Controllers/JorneyController.js
@@ -5,15 +5,29 @@ var jorneyApp = angular.module("jorneyApp", []);
 jorneyApp.controller("JorneyController", ["$scope", "$http", JorneyCtrl]);
 
 function JorneyCtrl ($scope, $http) {
-    $scope.busIds = null;
-    $scope.getBusIds = function () {
+    $scope.routes = null;
+    $scope.initRoutes = function () {
         $http.get("https://api.tfl.gov.uk/Line/Mode/bus?app_id=&app_key=")
         .success(function (data) {
-            $scope.busIds = data;
+            $scope.routes = data.slice(1, 101);
         })
         .error(function (data) {
             console.log(data);
         });
     }
-    
+    $scope.currentRouteCoord = null;
+    $scope.paintRoute = function (routeId) {
+        getRouteInfo(routeId);        
+    }
+
+    function getRouteInfo(routeId) {
+        $http.get("https://api.tfl.gov.uk/Line/" + routeId + "/Route/Sequence/ inbound?serviceTypes=regular&excludeCrowding=True&app_id=&app_key=")
+        .success(function (data) {
+            $scope.currentRouteCoord = JSON.parse(data.lineStrings[0]);
+            $scope.fsd = $scope.currentRouteCoord;
+        })
+        .error(function (data) {
+            console.log(data);
+        });
+    }
 }
