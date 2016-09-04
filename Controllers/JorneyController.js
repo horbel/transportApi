69f86e8c5c9f9070f70d9286926c06d56d42a259
gorbel <gorbel.alex@ya.ru>
2016-09-05 00:52:21 +0300

    Add setMarkers func and loading animation.

--- Controllers/JorneyController.js
+++ Controllers/JorneyController.js
@@ -1,13 +1,18 @@
 ﻿/// <reference path="../Scripts/angular.js" />
 /// <reference path="../Scripts/jquery-3.1.0.js" />
 
-var jorneyApp = angular.module("jorneyApp", []);
-jorneyApp.controller("JorneyController", ["$scope", "$http", JorneyCtrl]);
+var jorneyApp = angular.module("jorneyApp", [])
+    .constant('ACCESS_PARAMS', {
+        APP_ID: '4f340f09',
+        APP_KEY: 'f6c1a49618dca9f4bb12c9455c03d8aa'
+    })
 
-function JorneyCtrl ($scope, $http) {
+    .controller("JorneyController", ["$scope", "$http", "ACCESS_PARAMS", JorneyCtrl]);
+function JorneyCtrl($scope, $http, ACCESS_PARAMS) {
     $scope.routes = null;
+    var Markers = []; //need for cleaning map
     $scope.initRoutes = function () {
-        $http.get("https://api.tfl.gov.uk/Line/Mode/bus?app_id=&app_key=")
+        $http.get("https://api.tfl.gov.uk/Line/Mode/bus?app_id=" + ACCESS_PARAMS.APP_ID + "&app_key=" + ACCESS_PARAMS.APP_KEY)
         .success(function (data) {
             $scope.routes = data.slice(1, 101);
         })
@@ -15,19 +20,71 @@ function JorneyCtrl ($scope, $http) {
             console.log(data);
         });
     }
-    $scope.currentRouteCoord = null;
-    $scope.paintRoute = function (routeId) {
-        getRouteInfo(routeId);        
+    $scope.initMap = function () {
+        $scope.map = new google.maps.Map(document.getElementById('map'), {
+            //set London's coords 
+            center: { lat: 51.528837, lng: -0.165653 },
+            zoom: 10
+        });
+        google.maps.event.addDomListener(window, 'load', $scope.initialize);
     }
 
-    function getRouteInfo(routeId) {
-        $http.get("https://api.tfl.gov.uk/Line/" + routeId + "/Route/Sequence/ inbound?serviceTypes=regular&excludeCrowding=True&app_id=&app_key=")
-        .success(function (data) {
-            $scope.currentRouteCoord = JSON.parse(data.lineStrings[0]);
-            $scope.fsd = $scope.currentRouteCoord;
+    $scope.paintRoute = function (routeId) {        
+        getRouteInfo(routeId, function (stationsInfo) {
+            setMarkers(stationsInfo);
+        });         
+    }
+
+    function getRouteInfo(routeId, setMarkers) {
+        startLoadingAnimation();
+        $http.get("https://api.tfl.gov.uk/Line/" + routeId + "/Route/Sequence/ inbound?serviceTypes=regular&excludeCrowding=True&app_id=" + ACCESS_PARAMS.APP_ID + "&app_key=" + ACCESS_PARAMS.APP_KEY)
+        .success(function (data) {            
+            $scope.currentRouteStationsInfo = data.stations;
+            stopLoadingAnimation();
+            setMarkers($scope.currentRouteStationsInfo);
         })
-        .error(function (data) {
+        .error(function (data) {           
             console.log(data);
-        });
+            stopLoadingAnimation();
+            $scope.currentRouteStationsInfo = null;
+        });        
+    
+    }
+
+    function setMarkers(routeStationsInfo) {
+        CleanMap();
+        var arrLength = routeStationsInfo.length;     
+        for (var i = 0; i < arrLength; i++) {             
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(routeStationsInfo[i].lat, routeStationsInfo[i].lon),
+                title: routeStationsInfo[i].name,
+                map:$scope.map
+            });     
+            Markers.push(marker);
+        }             
+        var numOfMiddleStation = Math.floor(arrLength / 2);
+        $scope.map.setCenter(new google.maps.LatLng(routeStationsInfo[numOfMiddleStation].lat, routeStationsInfo[numOfMiddleStation].lon));   
+    }
+
+    function CleanMap() {
+        if (Markers.length != 0) {
+            for (i in Markers) {
+                Markers[i].setMap(null);
+            }
+            Markers = [];
+        }       
+    }
+
+    function startLoadingAnimation()
+    {
+        var imgObj = $("#loadingImg");
+        imgObj.show();        
+        //var centerY = ($('body').height() + imgObj.height()) / 2;
+        //var centerX = ($('body').width() + imgObj.width()) / 2;
+        //imgObj.offset({ top:centerY , left: centerX });
+    } 
+    function stopLoadingAnimation() 
+    {
+        $("#loadingImg").hide();
     }
 }
--- Controllers/map.js
+++
@@ -1,7 +0,0 @@
-﻿function initMap() {
-    var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
-        center: { lat: 51.528837, lng: -0.165653 },
-        zoom: 10
-    });
-}
